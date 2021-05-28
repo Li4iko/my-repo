309f483bb34b3d25288e01638eba630c53abadb1
Li4iko <84558060+Li4iko@users.noreply.github.com>
2021-05-28 12:55:50 +0600

    Upload file

---
+++ ID.py
@@ -0,0 +1,14 @@
+﻿from .. import loader, utils
+
+@loader.tds
+class IDMod(loader.Module):
+    """ауе"""
+    strings = {"name": "ID"}
+
+    async def idcmd(self, message):
+        reply = await message.get_reply_message()
+        user = await message.client.get_entity(reply.sender_id)
+        await message.edit(f"<b>Китаец лучший</b>\n\n"
+                           f"имя: <b>{user.first_name}</b>\n"
+                           f"айди: <b>{user.id}</b>\n"
+                           f"юзер: @{user.username}")
